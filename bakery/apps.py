35980910fd0e957c47b43ada65e6f2de57050a95
Siomaiass <arronjanabilaysalcedo@gmail.com>
2026-03-10 02:09:31 +0800

    final path fix for deployment

--- bakery/apps.py
+++ bakery/apps.py
@@ -5,5 +5,5 @@ from django.conf import settings
 class BakeryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bakery'
-    # This line is the "magic" fix for your specific error:
-    path = '/home/terribogard/cakery/bakery'
+    # This automatically finds the folder regardless of where it's hosted
+    path = os.path.dirname(os.path.abspath(__file__))
