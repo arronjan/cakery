fca077e253ce84f2310d63f52979a894f196963c
Siomaiass <arronjanabilaysalcedo@gmail.com>
2026-03-10 02:00:14 +0800

    fixed app path tp resolve multiple location error

--- bakery/apps.py
+++ bakery/apps.py
@@ -5,5 +5,5 @@ from django.conf import settings
 class BakeryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bakery'
-    # This line tells Django: "Look for the files right here in this folder"
-    path = os.path.join(settings.BASE_DIR, 'bakery')
+    # This line is the "magic" fix for your specific error:
+    path = '/home/terribogard/cakery/bakery'
