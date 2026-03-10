600dfbd5f29f798041b6725902f9cc1e880fcf72
Siomaiass <arronjanabilaysalcedo@gmail.com>
2026-03-10 15:03:38 +0800

    commit

--- bakery/models.py
+++ bakery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Your cake models will go here later!
+class Cake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='cake_pics/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
